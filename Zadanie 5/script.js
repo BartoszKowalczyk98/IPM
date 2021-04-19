7e725b2a197f66209583587bd0be637f1af9f43b
BartoszKowalczyk98 <kowalczykbar98@gmail.com>
2021-04-19 09:54:21 +0200

    added fields in html and at the beginning of js it now properly displays them Also started working on addrow function so that it works with new dataset
    Signed-off-by: BartoszKowalczyk98 <kowalczykbar98@gmail.com>

--- Zadanie 5/script.js
+++ Zadanie 5/script.js
@@ -13,9 +13,36 @@ if (!window.indexedDB) {
 }
 
 const employeeData = [
-    {id: "00-01", name: "gopal", age: 35, email: "gopal@tutorialspoint.com"},
-    {id: "00-02", name: "prasad", age: 32, email: "prasad@tutorialspoint.com"},
-    {id: "00-04", name: "BArtosz", age: 21, email: "kow@tsd.com"}
+    {
+        name: "gopal",
+        surname: "lapog",
+        age: 35,
+        dowod: "ABC123456",
+        email: "gopal@tutorialspoint.com",
+        phone: "123456789",
+        adres: "UL Piotrkowska 1 Warszawa",
+        kodpocztowy: "99-999"
+    },
+    {
+        name: "Piotr",
+        surname: "Nowak",
+        age: 60,
+        dowod: "PLK765021",
+        email: "lemon@tree.com",
+        phone: "718920128",
+        adres: "UL Wiecowskiego 1 Bydgoszcz",
+        kodpocztowy: "12-456"
+    },
+    {
+        name: "Jan",
+        surname: "Kowal",
+        age: 15,
+        dowod: "BCA109283",
+        email: "test@domain.com",
+        phone: "987654321",
+        adres: "UL Piotrkowska 1 Lodz",
+        kodpocztowy: "11-111"
+    }
 ];
 var db;
 var request = window.indexedDB.open("newDatabase", 1);
@@ -41,14 +68,39 @@ request.onsuccess = function (event) {
             td.style.minWidth = '150px'
 
             var td = tr.insertCell();
+            td.appendChild(document.createTextNode(cursor.value.surname))
+            td.style.border = '1px solid black';
+            td.style.minWidth = '150px'
+
+            var td = tr.insertCell();
             td.appendChild(document.createTextNode(cursor.value.age))
+            td.style.border = '1px solid black';
+            td.style.minWidth = '50px'
+
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(cursor.value.dowod))
             td.style.border = '1px solid black';
             td.style.minWidth = '50px'
 
             var td = tr.insertCell();
             td.appendChild(document.createTextNode(cursor.value.email))
             td.style.border = '1px solid black';
-            td.style.minWidth = '300px'
+            td.style.minWidth = '50px'
+
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(cursor.value.phone))
+            td.style.border = '1px solid black';
+            td.style.minWidth = '50px'
+
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(cursor.value.adres))
+            td.style.border = '1px solid black';
+            td.style.minWidth = '50px'
+
+            var td = tr.insertCell();
+            td.appendChild(document.createTextNode(cursor.value.kodpocztowy))
+            td.style.border = '1px solid black';
+            td.style.minWidth = '50px'
 
             var td = tr.insertCell();
             var btnremove = document.createElement('button')
@@ -58,7 +110,7 @@ request.onsuccess = function (event) {
             btnremove.textContent = 'usuń'
             td.appendChild(btnremove)
             td.style.border = '1px solid black';
-            td.style.minWidth = '50px'
+            td.style.minWidth = '30px'
 
             cursor.continue();
         }
@@ -67,7 +119,7 @@ request.onsuccess = function (event) {
 
 request.onupgradeneeded = function (event) {
     var db = event.target.result;
-    var objectStore = db.createObjectStore("employee", {keyPath: "id"});
+    var objectStore = db.createObjectStore("employee", {autoIncrement: true});
 
     for (var i in employeeData) {
         objectStore.add(employeeData[i]);
@@ -86,11 +138,20 @@ function tableCreate() {
     theadth = theadtr.insertCell()
     theadth.appendChild(document.createTextNode('name'))
     theadth = theadtr.insertCell()
+    theadth.appendChild(document.createTextNode('surname'))
+    theadth = theadtr.insertCell()
     theadth.appendChild(document.createTextNode('age'))
     theadth = theadtr.insertCell()
+    theadth.appendChild(document.createTextNode('ID number'))
+    theadth = theadtr.insertCell()
     theadth.appendChild(document.createTextNode('email'))
+    theadth = theadtr.insertCell()
+    theadth.appendChild(document.createTextNode('Phone number'))
+    theadth = theadtr.insertCell()
+    theadth.appendChild(document.createTextNode('Address'))
+    theadth = theadtr.insertCell()
+    theadth.appendChild(document.createTextNode('Postal code'))
     theadtr.insertCell()
-
 
     var tbody = document.createElement('tbody')
     tbody.id = 'dyntbody'
@@ -104,34 +165,36 @@ function addrow() {
     var request = db.transaction(["employee"], "readwrite")
         .objectStore("employee")
         .add({
-            id: document.getElementById('id').value, name: document.getElementById('name').value,
-            age: document.getElementById('age').value, email: document.getElementById('email').value
+            name: document.getElementById('name').value,
+            age: document.getElementById('age').value,
+            email: document.getElementById('email').value
         });
 
     request.onsuccess = function (event) {
         var tbody = document.getElementById('dyntbody')
         var tr = tbody.insertRow();
         var td = tr.insertCell();
+        // console.log()
         td.appendChild(document.createTextNode(document.getElementById('id').value))
         td.style.border = '1px solid black';
         td.style.minWidth = '50px'
-
         var td = tr.insertCell();
+
         td.appendChild(document.createTextNode(document.getElementById('name').value))
         td.style.border = '1px solid black';
         td.style.minWidth = '150px'
-
         var td = tr.insertCell();
+
         td.appendChild(document.createTextNode(document.getElementById('age').value))
         td.style.border = '1px solid black';
         td.style.minWidth = '50px'
-
         var td = tr.insertCell();
-        td.appendChild(document.createTextNode(document.getElementById('email').value))
+        td.appendChild(document.crea
+        teTextNode(document.getElementById('email').value))
         td.style.border = '1px solid black';
         td.style.minWidth = '200px'
-
         var td = tr.insertCell();
+
         var btnremove = document.createElement('button')
         btnremove.onclick = function () {
             remove(document.createTextNode(document.getElementById('id').value));
@@ -157,7 +220,7 @@ function remove(id) {
     request.onsuccess = function (event) {
         console.log(id)
     };
-    request.onerror= function (){
+    request.onerror = function () {
         console.log("error")
     }
 }
