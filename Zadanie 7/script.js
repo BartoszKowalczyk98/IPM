2995d943377dbb12bbdfedb63155ec1ba7a98d28
BartoszKowalczyk98 <kowalczykbar98@gmail.com>
2021-05-05 15:40:38 +0200

    task 7 initial commit with some code taken from w3 schools because i forgot to do a commit earlier
    Signed-off-by: BartoszKowalczyk98 <kowalczykbar98@gmail.com>

--- Zadanie 7/script.js
+++ Zadanie 7/script.js
@@ -519,3 +519,34 @@ function updateValue(e) {
         alert("Unable to edit data\r\n" + document.getElementById('id').value );
     }
 }
+
+function getDbObjects(filter = (object) => true) {
+    let ret = new Promise((res, rej) => {
+        let objects = [];
+        if (db) {
+            var store = db.transaction("employee", 'readonly').objectStore("employee");
+            store.openCursor().onsuccess = (e) => {
+                var c = e.target.result;
+                if (c) {
+                    if (filter(c.value)) {
+                        objects.push(c.value);
+                    }
+                    c.continue();
+                } else {
+                    res(objects);
+                }
+            };
+        }
+    });
+    return ret;
+}
+
+window.onload = () => {
+    worker = new Worker('worker.js');
+    const triggerWorkerButton = document.getElementById('TriggerWorker');
+    triggerWorkerButton.addEventListener('click', (e) => {
+        getDbObjects().then((value) => {
+            worker.postMessage(JSON.stringify(value))
+        });
+    });
+}
