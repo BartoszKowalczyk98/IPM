e782c7b9b8379666a265a52a80edaf77fb5211fc
BartoszKowalczyk98 <kowalczykbar98@gmail.com>
2021-05-05 16:23:21 +0200

    swapcase works but i have no idea what now
    Signed-off-by: BartoszKowalczyk98 <kowalczykbar98@gmail.com>

--- Zadanie 7/worker.js
+++ Zadanie 7/worker.js
@@ -1,3 +1,23 @@
 onmessage = function (e) {
-    console.log(JSON.parse(e.data));
+
+    JSON.parse(e.data).forEach(element => {
+        console.log(swapcase(element["name"]))
+        console.log(swapcase(element["surname"]))
+        console.log(swapcase(element["adres"]))
+        console.log(swapcase(element["email"]))
+    });
+    // console.log(JSON.parse(e.data));
 };
+
+
+function swapcase(str) {
+    var newWord =[];
+    for (i = 0; i < str.length; i++) {
+        if (str[i] == str[i].toLowerCase()) {
+            newWord[i] = str[i].toUpperCase();
+        } else {
+            newWord[i] = str[i].toLowerCase();
+        }
+    }
+    return newWord.join("");
+}
